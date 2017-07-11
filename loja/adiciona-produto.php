9b63eac2e07ec5dfef1be8767e75f3ac7f6a6fec
Marcello Batista <mbmartins95@gmail.com>
2017-07-11 00:33:40 -0300

    conexao com o banco

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -2,6 +2,21 @@
                 <?php 
                 $nome  = $_GET['nome'];
                 $preco = $_GET['preco'];
+
+                $conexao = mysqli_connect('localhost','root','root','loja');
+
+
+                $query = "insert into produtos (nome,preco) values ('{$nome}', {$preco})";
+
+               if(mysqli_query($conexao, $query)){
                 ?>
-              <p class="alert-success">  Produto <?php echo $nome,$preco ?> adicionado com sucesso!</p>
+                  <p class="alert-success">  Produto <?php echo $nome,$preco ?> adicionado com sucesso!</p>
+
+                 <?php   }else{ ?>
+                   <p class="alert-danger">  Produto <?php echo $nome ?> não adicionado com sucesso!</p>
+                <?php      } 
+
+                mysqli_close($conexao);
+                ?>
+            
   <?php include('rodape.php') ?>
